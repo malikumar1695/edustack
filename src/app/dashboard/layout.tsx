f7982565cb95461815d43cd8663763a85f92e7cc
Umer Sultan <osultan@islam.gov.qa>
2026-07-21 07:42:19 +0300

    Remove stale tracked path from earlier move

--- src/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AntdRegistry } from "@ant-design/nextjs-registry";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AntdRegistry>{children}</AntdRegistry>;
-}
